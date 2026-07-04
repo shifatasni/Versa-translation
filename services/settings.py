6cda809e943c993678d2ab118d6a547ff9390c7a
shifatasni <tasnishifa@gmail.com>
2026-07-04 21:23:13 +0530

    home

--- services/settings.py
+++ services/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'services.urls'
 
 TEMPLATES = [
--- static/script.js
+++ static/script.js
@@ -118,10 +118,37 @@ function goTop() {
 
 
 
-function openForm() {
-  document.getElementById("quoteForm").style.display = "block";
-}
 
+function openForm(type = "center", btn = null) {
+  const form = document.getElementById("quoteForm");
+
+  form.style.display = "block";
+
+  if (type === "center") {
+    form.style.position = "fixed";
+    form.style.top = "50%";
+    form.style.left = "50%";
+    form.style.transform = "translate(-50%, -50%)";
+  }
+
+  if (type === "nav" && btn) {
+    const rect = btn.getBoundingClientRect();
+
+    form.style.position = "fixed";
+    form.style.transform = "none";
+
+    // Get actual width after display
+    const formWidth = form.offsetWidth;
+
+    // Position below button
+    form.style.top = rect.bottom + 10 + "px";
+
+    // Center under button
+    const left = rect.left + (rect.width / 2) - (formWidth / 2);
+
+    form.style.left = left + "px";
+  }
+}
 function closeForm() {
   document.getElementById("quoteForm").style.display = "none";
 }
@@ -133,7 +160,7 @@ function sendToWhatsApp(event) {
   let language = document.getElementById("language").value;
   let message = document.getElementById("message").value;
 
-  let phoneNumber = "918590292015"; // your WhatsApp number
+  let phoneNumber = "918590292015";
 
   let url = "https://wa.me/" + phoneNumber + "?text="
     + encodeURIComponent(
@@ -148,7 +175,17 @@ function sendToWhatsApp(event) {
 
 
 
+function toggleForm(formId) {
+  let form = document.getElementById(formId);
 
+  // Close all forms first
+  document.querySelectorAll(".form-popup").forEach(f => {
+    if (f.id !== formId) f.style.display = "none";
+  });
+
+  // Toggle selected form
+  form.style.display = (form.style.display === "block") ? "none" : "block";
+}
 
 
 function toggleMenu() {
