783292e441f7e120f57961d5cdc2162a08c19ff3
shifatasni <tasnishifa@gmail.com>
2026-07-06 22:39:34 +0530

    new

--- static/script.js
+++ static/script.js
@@ -111,7 +111,17 @@ function goTop() {
   });
 }
 
+// Get button
+let topBtn = document.getElementById("topBtn");
 
+// Show button when scrolling
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
 
 
 
